78454f8d5eff3e3f20a6b8fd13a8db63c2b7b3c5
Toru Ito <design@designnoMacBook-Pro.local>
2017-03-06 21:50:55 +0900

    Add texture display

--- FontDraw/main.cpp
+++ FontDraw/main.cpp
@@ -20,12 +20,74 @@
 
 #include "ShaderProgram.hpp"
 #include "ShapeData.hpp"
+#include "TextureData.hpp"
 
 #include "FontData.hpp"
 
-
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
+    GLFWwindow* window;
     
+    // Initialize the library
+    if (!glfwInit())
+        return -1;
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow( 300, 300, "FontDraw", NULL, NULL );
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+    
+    //Font
+    FontData font;
+    if( !font.Init( "/Library/Fonts/Arial Unicode.ttf" ) )
+        return -1;
+    
+    //texture
+    Eigen::Matrix< unsigned char, Eigen::Dynamic, Eigen::Dynamic > image;
+    if( !font.CreateBitmap( L"アイウエオ", 50, image ))
+        return -1;
+    
+    ShaderProgram TexProgram;
+    TexProgram.InitProgramTexture( image );
+    
+    TextureData texture;
+    texture.InitDraw( (int)image.cols(), (int)image.rows() );
+    
+    font.Destroy();
+    
+    // Loop until the user closes the window
+    while( !glfwWindowShouldClose( window ) ) {
+        
+        // Render
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        texture.Draw();
+        glBindVertexArray( 0 );
+        
+        // Swap front and back buffers
+        glfwSwapBuffers( window );
+        
+        // Poll for and process events
+        glfwPollEvents();
+        
+    }
+    
+    texture.DeleteDraw();
+    TexProgram.DeleteProgram();
+    
+    glfwTerminate();
+    
+    /*
     GLFWwindow* window;
     
     // Initialize the library
@@ -58,7 +120,7 @@ int main(int argc, const char * argv[]) {
         return -1;
     
     std::vector< std::vector< std::vector< Eigen::Vector2d > > > OutlinePointList;
-    if( !font.CreateOutlinePoint( L"abcde あいうえお", &OutlinePointList ) )
+    if( !font.CreateOutlinePoint( L"アイウエオ", &OutlinePointList ) )
         return -1;
     
     ShapeData data;
@@ -68,7 +130,7 @@ int main(int argc, const char * argv[]) {
     
     //setting
     program.SetColor( 0.0f, 1.0f, 0.0f );
-    program.SetDataMatrix( &data );
+    program.SetDataMatrix( &data, Eigen::Vector3f::Zero() );
 
     data.InitDraw();
     
@@ -93,6 +155,7 @@ int main(int argc, const char * argv[]) {
     data.DeleteDraw();
     
     glfwTerminate();
+    */
     
     return 0;
 }
